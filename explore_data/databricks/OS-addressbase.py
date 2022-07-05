adf258c7c5e9bc71f7fbb2d145e74043154e8b74
lindacmsheard <lindacmsheard>
2022-07-05 08:35:38 +0000

    addressbase initial

--- explore_data/databricks/OS-addressbase.py
+++ explore_data/databricks/OS-addressbase.py
@@ -1,9 +1,15 @@
 # Databricks notebook source
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC # Addressbase sample
 # MAGIC 
 # MAGIC - obtained by signing up here: https://www.ordnancesurvey.co.uk/business-government/products/addressbase
-# MAGIC - zip file uploaded to landing blob storage, and then unzipped to bronze zone of datalake using ADF.
+# MAGIC - zip file uploaded to landing blob storage, and then unzipped to bronze zone of datalake using Azure Data Factory.
 # MAGIC 
 # MAGIC No header in sample. Header downloaded separately from https://www.ordnancesurvey.co.uk/business-government/tools-support/addressbase-support (accessed 2022-04-24)
 # MAGIC 
@@ -16,6 +22,20 @@
 # MAGIC ```
 # MAGIC 
 # MAGIC Technical Specification: https://www.ordnancesurvey.co.uk/documents/product-support/tech-spec/addressbase-technical-specification.pdf
+# MAGIC 
+# MAGIC #### About the sample
+# MAGIC - 42861 records
+# MAGIC - Location: Exeter
+# MAGIC - CRS (Lat and long columns): ETRS89 (https://epsg.io/4258)
+# MAGIC - CRS (X and Y columns): BNG (https://epsg.io/27700)
+# MAGIC 
+# MAGIC This image shows a 10% sample of the data within the sample bounding box:
+# MAGIC 
+# MAGIC ![addressbase sample area](https://github.com/azuregig/work_with_OrdnanceSurvey_data/raw/dev/addressbase/explore_data/img/addressbase-10pc.png)
+# MAGIC 
+# MAGIC This image shows the a small area of the full dataset:
+# MAGIC 
+# MAGIC ![addressbase](https://github.com/azuregig/work_with_OrdnanceSurvey_data/raw/dev/addressbase/explore_data/img/addressbase-full.png)
 
 # COMMAND ----------
 
@@ -27,6 +47,7 @@ dbutils.fs.ls(f'/mnt/bronze/{dataset}')
 
 from pyspark.sql.types import *
 
+# construct schema from the information in the separate header file
 schema = StructType([
     StructField("UPRN",StringType(), True),
     StructField("OS_ADDRESS_TOID",StringType(), True),
@@ -72,10 +93,35 @@ print(f'The sample contains {records} records.')
 
 # COMMAND ----------
 
-# collect a sample for display
+# MAGIC %md
+# MAGIC ## Single-threaded exploration of a sample
+# MAGIC 
+# MAGIC When we call `toPandas()` on a spark dataframe, the data is collected onto the driver node. Any further processing on the pandas dataframe will proceed on the driver node only. We can use standard python libraries such as `folium` and `geopandas` on this sample.
 
-pdf = df.sample(0.3).toPandas()
+# COMMAND ----------
+
+# collect a random sample for display
+
+pdf = df.sample(0.001).toPandas()
 print(f'Collecting {len(pdf)} records to the driver node. ({type(pdf)})')
+
+# COMMAND ----------
+
+# or frame a smaller sample for display
+# using non-geospatially aware filtering
+
+# pdf = df.filter(col("POSTCODE").like('EX1 3%')).toPandas()
+                 
+# print(f'Collecting {len(pdf)} records to the driver node. ({type(pdf)})')
+
+# COMMAND ----------
+
+# or filter naively by lat long (not using any geospatial tooling here)
+
+# ymin, ymax, xmin,xmax = [50.7227, 50.7327, -3.5461, -3.5268]
+# pdf = df.filter(col("LATITUDE").between(ymin, ymax) & col("LONGITUDE").between(xmin,xmax)).toPandas()
+                 
+# print(f'Collecting {len(pdf)} records to the driver node. ({type(pdf)})')
 
 # COMMAND ----------
 
@@ -84,7 +130,7 @@ import geopandas as gpd
 
 gdf = gpd.GeoDataFrame(
     pdf, geometry=gpd.points_from_xy(pdf.LONGITUDE, pdf.LATITUDE))
-gdf.head()
+gdf.head(1)
 
 # COMMAND ----------
 
@@ -106,6 +152,10 @@ bounds = [[y1,x1],[y1,x2], [y2,x2],[y2,x1]]
 m = folium.Map(location=[points.geometry.representative_point().y.mean(), points.geometry.representative_point().x.mean()],
                zoom_start = 15, width = '80%', height = '100%')
 
+# ensure we can click the map to get a lat long
+m.add_child(folium.LatLngPopup())
+
+
 folium.GeoJson(points, 
                marker = folium.CircleMarker(radius = 3, # Radius in metres
                                            weight = 0, #outline weight
@@ -113,7 +163,7 @@ folium.GeoJson(points,
                                            fill_opacity = 1),
                style_function = lambda x: {"color":"red", "weight":1, "opacity":1}, 
                name="addressbase", 
-               tooltip = folium.features.GeoJsonTooltip(fields=['UPRN', "BUILDING_NUMBER", "THOROUGHFARE"])
+               tooltip = folium.features.GeoJsonTooltip(fields=['UPRN', "BUILDING_NUMBER", "THOROUGHFARE", "POSTCODE"])
               ).add_to(m)
 
 folium.vector_layers.Polygon(bounds).add_to(m)    
